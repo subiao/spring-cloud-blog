5b59dc38508dbfd07dbc142ee051e81bdd03e256
sub <791923958@qq.com>
2021-05-30 17:36:15 +0800

    1.pom父模块建立 2.支付demo建立

--- cloud-provider-payment8001/src/main/java/com/sub/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/sub/controller/PaymentController.java
@@ -26,7 +26,7 @@ public class PaymentController {
     @PostMapping(value = "/payment/create")
     public CommonResult create(Payment payment) {
         int result = paymentService.create(payment);
-        log.info("插入结果{0}", payment);
+        log.info("插入结果{}", payment);
         if (result > 0) {
             return new CommonResult(200, "插入数据成功！",payment);
         } else {
@@ -37,7 +37,7 @@ public class PaymentController {
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
-        log.info("查询结果{0}", payment);
+        log.info("查询结果{}", payment);
         if (payment != null) {
             return new CommonResult(200, "查询成功！", payment);
         } else {
