63e6ab243dd0703eba109478434349d5e48b8de4
sub <791923958@qq.com>
2021-05-31 18:13:36 +0800

    1.加入eurakServer 2.注册order服务 3.注册payment服务

--- cloud-euraka-server7001/src/main/java/com/sub/EurakaServer7001.java
+++ cloud-euraka-server7001/src/main/java/com/sub/EurakaServer7001.java
@@ -2,17 +2,18 @@ package com.sub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author sub
- * @Date 2021/5/30 16:03
+ * @Date 2021/5/31 17:43
  * @Version 1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurakaServer7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurakaServer7001.class, args);
     }
+
 }
