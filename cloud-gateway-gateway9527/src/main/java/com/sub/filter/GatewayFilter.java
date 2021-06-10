783a3bf91ef17343b4e2bff82f300958546123a5
sub <791923958@qq.com>
2021-06-10 09:37:34 +0800

    1.网关添加filter用于进行日志记录、统一鉴权

---
+++ cloud-gateway-gateway9527/src/main/java/com/sub/filter/GatewayFilter.java
@@ -0,0 +1,36 @@
+package com.sub.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author sub
+ * @Date 2021/6/10 9:30
+ * @Version 1.0
+ */
+@Component
+@Slf4j
+public class GatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("*************************come in*******************");
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if(uname == null){
+            log.info("用户名为空！");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return  exchange.getResponse().setComplete();
+        }
+        return  chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
