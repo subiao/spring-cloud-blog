a1142731d39cb0f387bd2f6719eb318405ece11f
sub <791923958@qq.com>
2021-06-23 16:39:27 +0800

    1.@sentinelResource通过fallback 限流降级

---
+++ cloud-alibaba-provider-9001/src/main/java/com/sub/controller/FallBackController.java
@@ -0,0 +1,34 @@
+package com.sub.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.sub.entity.CommonResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author sub
+ * @Date 2021/6/23 16:25
+ * @Version 1.0
+ */
+@RestController
+public class FallBackController {
+    @RequestMapping("/fallback/{id}")
+    @SentinelResource(value = "back", fallback = "back")
+    public CommonResult fallback(@PathVariable Long id) {
+        if (id == 4) {
+            throw new IllegalArgumentException("参数异常");
+        } else if (id == 5) {
+            throw new NullPointerException("空指针异常");
+        }
+        return new CommonResult(200, "正常降级！");
+    }
+
+    public  static CommonResult back(@PathVariable Long id, Throwable e){
+
+        return new CommonResult(200,"降级id"+id+"降级异常类"+e.getMessage());
+    }
+
+}
